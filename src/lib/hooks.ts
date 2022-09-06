be91baa0002885f9ff531d31caf24ae1932bf637
Gus Ryan <gus.ryan163@gmail.com>
2022-09-06 15:09:19 -0700

    removed unused functionality

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,5 +1,4 @@
 import { useEffect } from "react"
-import Router from "next/router"
 
 import useSWR from "swr"
 import axios from "axios"
@@ -7,11 +6,9 @@ import { getCookie } from "cookies-next"
 
 import { User } from "../models/user"
 
-const fetcher = async (url: string) => {
-	return (await axios.get(url)).data
-}
+const fetcher = async (url: string) => (await axios.get(url)).data
 
-export function useUser({ redirectTo = "", redirectIfFound = false } = {}) {
+export function useUser() {
 	const {
 		data,
 		mutate: mutateUser,
@@ -20,6 +17,8 @@ export function useUser({ redirectTo = "", redirectIfFound = false } = {}) {
 		onErrorRetry(error, key, config, revalidate, { retryCount }) {
 			// never retry on 401
 			if (error.response.status === 401) return
+
+			console.log(error)
 
 			// Only retry up to 10 times.
 			if (retryCount >= 10) return
@@ -39,20 +38,26 @@ export function useUser({ redirectTo = "", redirectIfFound = false } = {}) {
 		}
 	}, [])
 
-	useEffect(() => {
-		// if no redirect needed, just return (example: already on /dashboard)
-		// if user data not yet there (fetch in progress, logged in or not) then don't do anything yet
-		if (!redirectTo || !swrUser) return
-
-		if (
-			// If redirectTo is set, redirect if the user was not found.
-			(redirectTo && !redirectIfFound && !swrUser) ||
-			// If redirectIfFound is also set, redirect if the user was found
-			(redirectIfFound && swrUser)
-		) {
-			Router.push(redirectTo)
-		}
-	}, [swrUser, redirectIfFound, redirectTo])
-
 	return { user: swrUser || cookieUser, mutateUser, error }
+}
+
+export function useAtPageBottom(
+	cb: (offset?: number) => void,
+	offset: number = 0,
+	dependencies: any
+): void {
+	useEffect(() => {
+		const scrollListener = () => {
+			if (
+				window.scrollY + window.innerHeight + offset + 1 >=
+				document.body.scrollHeight
+			) {
+				cb(document.body.scrollHeight - window.scrollY + window.innerHeight)
+			}
+		}
+		scrollListener()
+
+		window.addEventListener("scroll", scrollListener)
+		return () => window.removeEventListener("scroll", scrollListener) // clean up
+	}, dependencies)
 }
