e623debf1f9f181a25bb155ceaf949713285ff40
Gus Ryan <gus.ryan163@gmail.com>
2022-09-06 21:43:46 -0700

    add capitalise function

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -2,3 +2,6 @@ export type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>
 
 type Anyify<T> = { [P in keyof T]?: any }
 export type Modify<T, R extends Anyify<T>> = Omit<T, keyof R> & R
+
+export const capitalise = (word: string): string =>
+	word[0].toUpperCase() + word.slice(1)
