6298932942ac76da7d35bc0b90b9f40cd3a203a2
Gus Ryan <gus.ryan163@gmail.com>
2022-10-17 20:02:02 -0400

    add user pages

--- src/pages/api/timers/^get.ts
+++ src/pages/api/timers/^get.ts
@@ -2,7 +2,12 @@ import type { NextApiRequest, NextApiResponse } from "next"
 import { isValidDate } from "../../../lib/date"
 
 import ServerError from "../../../lib/error"
-import { getByEndTime, getByTimeCreated } from "../../../models/timer"
+import {
+	getByEndTime,
+	getByTimeCreated,
+	getFromUserByEndTime,
+	getFromUserByTimeCreated,
+} from "../../../models/timer"
 
 export default async function getTimers(
 	req: NextApiRequest,
@@ -12,11 +17,25 @@ export default async function getTimers(
 		throw new ServerError("Sort must be included and not an array", 400)
 	if (Array.isArray(req.query.offset))
 		throw new ServerError("Offset must not be an array", 400)
+	if (Array.isArray(req.query.userid))
+		throw new ServerError("Offset must not be an array", 400)
 
-	const { sort } = req.query
+	const { sort, userid } = req.query
 	const offset = req.query.offset ? new Date(req.query.offset) : undefined
 	if (offset && !isValidDate(offset))
 		throw new ServerError("Offset date is invalid", 400)
+
+	if (userid) {
+		if (sort === "enddate") {
+			const timers = await getFromUserByEndTime(userid, offset)
+			return res.status(200).json({ timers })
+		}
+
+		if (sort === "created") {
+			const timers = await getFromUserByTimeCreated(userid, offset)
+			return res.status(200).json({ timers })
+		}
+	}
 
 	if (sort === "enddate") {
 		const timers = await getByEndTime(offset)
---
+++ src/pages/user/[id].tsx
@@ -0,0 +1,106 @@
+import { useMemo } from "react"
+import Head from "next/head"
+
+import type { TimerLite, SerialisedTimer } from "../../models/timer"
+import { deSerialiseTimer } from "../../lib/serialise"
+import { getTimersFromApiServer } from "../../lib/util"
+import { makeGridWithFilters, FilterMap } from "../../components/grid"
+
+export default function UserPage({
+	serialisedTimers,
+	user,
+}: {
+	serialisedTimers: SerialisedTimer[]
+	user: User
+}): JSX.Element {
+	const initialTimers = serialisedTimers.map(deSerialiseTimer)
+	const Grid = useMemo(() => {
+		const initialFilter = "Default (end date)"
+		const filterMap = new FilterMap([
+			[
+				initialFilter,
+				{
+					getNewTimers: (offset?: string): Promise<TimerLite[]> =>
+						getTimersFromApiServer({
+							offset,
+							userid: user.id,
+							sort: "enddate",
+						}),
+					getTimerOffset: (timer: TimerLite): string =>
+						new Date(timer.endTime).toISOString(),
+				},
+			],
+			[
+				"Created at",
+				{
+					getNewTimers: (offset?: string): Promise<TimerLite[]> =>
+						getTimersFromApiServer({
+							offset,
+							userid: user.id,
+							sort: "created",
+						}),
+					getTimerOffset: (timer: TimerLite): string =>
+						new Date(timer.createdAt).toISOString(),
+				},
+			],
+		])
+
+		return makeGridWithFilters(filterMap, initialFilter, initialTimers)
+	}, [initialTimers, user])
+
+	return (
+		<>
+			<Head>
+				<title>{`Countdown timer | ${user.displayName}'s timers`}</title>
+			</Head>
+			<Grid />
+		</>
+	)
+}
+
+// static props
+
+import { GetStaticProps, GetStaticPaths } from "next"
+
+import { getFromUserByEndTime } from "../../models/timer"
+import { findById, getAllIds, User } from "../../models/user"
+import { serialiseTimer } from "../../lib/serialise"
+
+export const getStaticPaths: GetStaticPaths = async () => {
+	const ids = await getAllIds()
+	const paths = ids.map((id) => ({ params: { id } }))
+
+	return {
+		paths,
+		fallback: "blocking",
+	}
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+	console.log(`[Next.js] running getStaticProps on path /user/${params?.id}`)
+	if (
+		params === undefined ||
+		params.id === "undefined" ||
+		Array.isArray(params.id) ||
+		params.id === undefined
+	) {
+		return { notFound: true }
+	}
+
+	const userId = params.id
+	const [timers, user] = await Promise.all([
+		getFromUserByEndTime(userId),
+		findById(userId),
+	])
+	if (!user) {
+		console.log(`[Next.js] path ${params.id} not found`)
+		return { notFound: true }
+	}
+
+	return {
+		props: {
+			serialisedTimers: timers.map(serialiseTimer),
+			user,
+		},
+	}
+}
